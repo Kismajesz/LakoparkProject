6462f11082206860a824bd8808ac9d845ba21792
Zalán Bölkény <Kismajesz@gmail.com>
2020-03-20 15:31:55 +0100

    1.feladat

---
+++ LakoparkProject/HappyLiving.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LakoparkProject
+{
+    class HappyLiving
+    {
+        List<lakopark> lakoparkok = new List<lakopark>();
+
+        public HappyLiving(List<lakopark> lakoparkok)
+        {
+            this.lakoparkok = lakoparkok;
+        }
+
+        internal List<lakopark> Lakoparkok { get => lakoparkok;}
+    }
+}
---
+++ LakoparkProject/lakopark.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LakoparkProject
+{
+    class lakopark
+    {
+        private int[,] hazak;
+        private int maxHazSzam;
+        private string nev;
+        private int utcakSzama;
+
+        public lakopark(int[,] hazak, int maxHazSzam, string nev, int utcakSzama)
+        {
+            this.hazak = hazak;
+            this.maxHazSzam = maxHazSzam;
+            this.nev = nev;
+            this.utcakSzama = utcakSzama;
+        }
+
+        public int[,] Hazak { get => hazak;  }
+        public int MaxHazSzam { get => maxHazSzam;  }
+        public string Nev { get => nev; }
+        public int UtcakSzama { get => utcakSzama; }
+    }
+}
